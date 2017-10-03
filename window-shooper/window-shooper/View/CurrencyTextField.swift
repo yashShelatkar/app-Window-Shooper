b582758789dd661aacfbeb51000529f11cfefd14
Yash <shelatkaryash@gmail.com>
2017-10-03 16:13:37 +1030

    Version1 Done

--- window-shooper/window-shooper/View/CurrencyTextField.swift
+++ window-shooper/window-shooper/View/CurrencyTextField.swift
@@ -11,6 +11,23 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLable = UILabel(frame:CGRect(x:5 , y: (frame.size.height/2) - size / 2 , width: size , height: size))
+       currencyLable.backgroundColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 0.3024668236)
+        currencyLable.textAlignment = .center
+        currencyLable.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        currencyLable.layer.cornerRadius = 5.0
+        currencyLable.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLable.text = formatter.currencySymbol
+        addSubview(currencyLable)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -37,7 +54,7 @@ class CurrencyTextField: UITextField {
                 let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
                 attributedPlaceholder = place
                 textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-                
+                clipsToBounds = true
                 
             }
             
